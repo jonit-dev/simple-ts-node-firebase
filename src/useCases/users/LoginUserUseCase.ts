687aef734731bb2de3d95a5d31c8078d6fd4f8da
Joao Paulo Furtado <joaopaulofurtado@live.com>
2021-08-08 16:29:36 -0700

    login rollback

--- src/useCases/users/LoginUserUseCase.ts
+++ src/useCases/users/LoginUserUseCase.ts
@@ -1,5 +1,5 @@
 class LoginUserUseCase {
-  public async login(username: string, password: string) {}
+  public async login(email: string, password: string) {}
 }
 
 const loginUserUseCase = new LoginUserUseCase();
